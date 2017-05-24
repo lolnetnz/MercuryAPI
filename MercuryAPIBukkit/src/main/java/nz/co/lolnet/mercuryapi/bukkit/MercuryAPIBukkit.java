673fbef98fdf600fad2da8da7d6a667dcb05d411
Alex Thomson <alexrobthomson@outlook.com>
2017-05-24 22:23:09 +1200

    Bukkit plugin

---
+++ MercuryAPIBukkit/src/main/java/nz/co/lolnet/mercuryapi/bukkit/MercuryAPIBukkit.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 lolnet.co.nz
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.co.lolnet.mercuryapi.bukkit;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import nz.co.lolnet.mercuryapi.bukkit.config.Config;
+import nz.co.lolnet.mercuryapi.bukkit.util.Reference;
+import nz.co.lolnet.mercuryapi.core.MercuryAPICore;
+
+public class MercuryAPIBukkit extends JavaPlugin {
+	
+	private static MercuryAPIBukkit instance;
+	private Config config;
+	private MercuryAPICore mercuryAPICore;
+	
+	public MercuryAPIBukkit() {
+		instance = this;
+	}
+	
+	@Override
+	public void onEnable() {
+		config = new Config();
+		config.loadConfig();
+		mercuryAPICore = new MercuryAPICore();
+		getMercuryAPICore().setAccount(getConfiguration().getString("MercuryAPIBukkit.UniqueId"), getConfiguration().getString("MercuryAPIBukkit.Token"));
+		getLogger().info(Reference.PLUGIN_NAME + " v" + Reference.VERSION + " loaded.");
+	}
+	
+	@Override
+	public void onDisable() {
+		mercuryAPICore = null;
+		config = null;
+		getLogger().info(Reference.PLUGIN_NAME + " v" + Reference.VERSION + " unloaded.");
+	}
+	
+	public static MercuryAPIBukkit getInstance() {
+		return instance;
+	}
+	
+	public YamlConfiguration getConfiguration() {
+		return config.getConfiguration();
+	}
+	
+	public MercuryAPICore getMercuryAPICore() {
+		return mercuryAPICore;
+	}
+}
---
+++ MercuryAPIBukkit/src/main/java/nz/co/lolnet/mercuryapi/bukkit/config/Config.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 lolnet.co.nz
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.co.lolnet.mercuryapi.bukkit.config;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.google.common.io.ByteStreams;
+
+import nz.co.lolnet.mercuryapi.bukkit.MercuryAPIBukkit;
+
+public class Config {
+	
+	private YamlConfiguration configuration;
+	
+	public void loadConfig() {
+		if (MercuryAPIBukkit.getInstance().getDataFolder().exists()) {
+			MercuryAPIBukkit.getInstance().getDataFolder().mkdir();
+		}
+		
+		configuration = loadFile("config.yml");
+	}
+	
+	public YamlConfiguration loadFile(String name) {
+		try {
+			File file = new File(MercuryAPIBukkit.getInstance().getDataFolder(), name);
+			
+			if (!file.exists()) {
+				file.createNewFile();
+				InputStream inputStream = MercuryAPIBukkit.getInstance().getClass().getResourceAsStream(name);
+				OutputStream outputStream = new FileOutputStream(file);
+				ByteStreams.copy(inputStream, outputStream);
+				MercuryAPIBukkit.getInstance().getLogger().info("Successfully created " + name);
+			}
+			
+			return YamlConfiguration.loadConfiguration(file);
+		} catch (IOException | NullPointerException | SecurityException ex) {
+			MercuryAPIBukkit.getInstance().getLogger().severe("Encountered an error processing 'loadFile' in '" + getClass().getSimpleName() + "' - " + ex.getMessage());
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	public YamlConfiguration getConfiguration() {
+		return configuration;
+	}
+}
